2167ccc4a7b3b0c41e5a456304d1a3f86d9c3a28
Chris Schneider <chris@scoutapp.com>
2017-03-27 14:13:38 -0600

    Fix mismatched file names for EExEngine and ExsEngine

--- lib/scout_apm/instruments/eex_engine.ex
+++ lib/scout_apm/instruments/eex_engine.ex
@@ -1,4 +1,4 @@
-defmodule ScoutApm.Instruments.ExsEngine do
+defmodule ScoutApm.Instruments.EExEngine do
   @behaviour Phoenix.Template.Engine
 
   # TODO: Make this name correctly for other template locations
@@ -8,10 +8,10 @@ defmodule ScoutApm.Instruments.ExsEngine do
                   |> Enum.drop(2)      # [page, index.html.eex]
                   |> Enum.join("/")    # "page/index.html.eex"
 
-    quoted_template = Phoenix.Template.ExsEngine.compile(path, name)
+    quoted_template = Phoenix.Template.EExEngine.compile(path, name)
 
     quote do
-      ScoutApm.TrackedRequest.start_layer("Exs", unquote(scout_name))
+      ScoutApm.TrackedRequest.start_layer("EEx", unquote(scout_name))
       result = unquote(quoted_template)
       ScoutApm.TrackedRequest.stop_layer
       result
--- lib/scout_apm/instruments/exs_engine.ex
+++ lib/scout_apm/instruments/exs_engine.ex
@@ -1,4 +1,4 @@
-defmodule ScoutApm.Instruments.EExEngine do
+defmodule ScoutApm.Instruments.ExsEngine do
   @behaviour Phoenix.Template.Engine
 
   # TODO: Make this name correctly for other template locations
@@ -8,10 +8,10 @@ defmodule ScoutApm.Instruments.EExEngine do
                   |> Enum.drop(2)      # [page, index.html.eex]
                   |> Enum.join("/")    # "page/index.html.eex"
 
-    quoted_template = Phoenix.Template.EExEngine.compile(path, name)
+    quoted_template = Phoenix.Template.ExsEngine.compile(path, name)
 
     quote do
-      ScoutApm.TrackedRequest.start_layer("EEx", unquote(scout_name))
+      ScoutApm.TrackedRequest.start_layer("Exs", unquote(scout_name))
       result = unquote(quoted_template)
       ScoutApm.TrackedRequest.stop_layer
       result
