ba1b76c93e0b4b4cf425ac616d85dd100d677550
Chris Schneider <chris@scoutapp.com>
2017-04-17 11:20:47 -0600

    Loosen many of the dependency specs
    We make pretty light use of some of the dependencies, and this library
    should try not to be a limiting factor on other apps

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ScoutApm.Mixfile do
 
   def project do
     [app: :scout_apm,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -14,9 +14,6 @@ defmodule ScoutApm.Mixfile do
 
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications:
@@ -30,29 +27,26 @@ defmodule ScoutApm.Mixfile do
      mod: {ScoutApm.Application, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:plug, "~>1.0"},
-      {:poison, "~> 2.0"},
-      {:hackney, "~> 1.6"},
-      {:timex, "~> 3.0"},
-      {:approximate_histogram, "~>0.1.0"},
 
-      # Release Tools
-      {:ex_doc, ">= 0.0.0", only: :dev},
+      # We only use `encode!(map)`, which has exited since the start of poison,
+      # so don't restrict the version of poison here. In the unlikely case that
+      # the encode! function is removed in a new version, we'll have to revisit.
+      {:poison, ">= 0.0.0"},
+
+      # We only use `request/5` from hackney, which hasn't changed in the 1.0 line.
+      {:hackney, "~> 1.0"},
+
+      {:timex, "~> 3.0"},
+
+      {:approximate_histogram, "~>0.1.1"},
 
       #########################
       # Dev & Testing Deps
 
+      {:ex_doc, ">= 0.0.0", only: [:dev]},
       {:propcheck, "~> 0.0", only: [:dev, :test]},
       {:credo, "~> 0.5", only: [:dev, :test]},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
