b9aa830de839052285b93e870e18dc59159a6f61
Chris Schneider <chris@scoutapp.com>
2017-06-27 12:31:55 -0600

    Bump VERSION to 0.3.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ScoutApm.Mixfile do
 
   def project do
     [app: :scout_apm,
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
