96ea57e98eccb444dcbc0fb2af03e5ac86e80b84
Mitchell Henke <mitchh23@gmail.com>
2018-02-06 15:05:56 -0600

    update package version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ScoutApm.Mixfile do
 
   def project do
     [app: :scout_apm,
-     version: "0.3.3",
+     version: "0.4.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
