4d1bf71502ba63d672fb2e1cf5f66b17cc5ff35a
Mitchell Henke <mitchh23@gmail.com>
2018-05-15 13:22:34 -0500

    add tests

---
+++ test/support/test_plug_app.ex
@@ -0,0 +1,28 @@
+defmodule ScoutApm.TestPlugApp do
+  use Plug.Router
+
+  plug :match
+  plug :add_private_phoenix_controller
+  plug ScoutApm.Plugs.ControllerTimer
+  plug :dispatch
+
+  get "/" do
+    put_private(conn, :phoenix_action, :index)
+    |> send_resp(200, "")
+  end
+
+  get "/500" do
+    put_private(conn, :phoenix_action, :"500")
+    |> send_resp(500, "")
+  end
+
+  get "/x-forwarded-for" do
+    put_req_header(conn, "x-forwarded-for", "1.2.3.4")
+    |> put_private(:phoenix_action, :"x-forwarded-for")
+    |> send_resp(200, "")
+  end
+
+  def add_private_phoenix_controller(conn, _opts) do
+    put_private(conn, :phoenix_controller, Elixir.MyTestApp.PageController)
+  end
+end
