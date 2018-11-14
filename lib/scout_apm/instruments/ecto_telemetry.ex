3ccc791b034fd3ba8668131c7deab2eb2fcaeb72
Mitchell Henke <mitchh23@gmail.com>
2018-11-14 10:45:40 -0600

    telemetry implementation for Ecto 3

---
+++ lib/scout_apm/instruments/ecto_telemetry.ex
@@ -0,0 +1,21 @@
+defmodule ScoutApm.Instruments.EctoTelemetry do
+  @doc """
+  Attaches an event handler for Ecto queries.
+
+  Takes a fully namespaced Ecto.Repo module as the only argument. Example:
+
+      ScoutApm.Instruments.EctoTelemetry.attach(MyApp.Repo)
+  """
+  def attach(repo_module) do
+    query_event = repo_module
+                  |> Module.split()
+                  |> Enum.map(& &1 |> Macro.underscore() |> String.to_atom())
+                  |> Kernel.++([:query])
+
+    Telemetry.attach("scout-ecto-query-handler", query_event, ScoutApm.Instruments.EctoTelemetry, :handle_event, nil)
+  end
+
+  def handle_event([_app, _repo, :query], _value, metadata, _config) do
+    ScoutApm.Instruments.EctoLogger.record(metadata)
+  end
+end
