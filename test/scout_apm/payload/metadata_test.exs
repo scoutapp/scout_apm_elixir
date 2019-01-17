77ac714c3346da9274000dffee9fbbfeb54ec2f9
FatboyPunk <marcel.horlings@gmail.com>
2019-01-17 11:39:18 +0100

    use a working function in the test for datetime in elixir 1.4

--- test/scout_apm/payload/metadata_test.exs
+++ test/scout_apm/payload/metadata_test.exs
@@ -6,12 +6,13 @@ defmodule ScoutApm.Payload.MetadataTest do
   describe "new/1" do
     test "creating a metadata struct with a timestamp" do
       {:ok, app_root} = File.cwd()
-      timestamp = DateTime.utc_now()
+      {:ok, timestamp} = NaiveDateTime.new(2018, 1, 1, 1, 5, 3)
 
       assert %ScoutApm.Payload.Metadata{
                agent_version: "0.4.9",
                app_root: ^app_root,
                payload_version: 1,
+               agent_time: "2018-01-01T01:05:03Z",
                platform: "elixir"
              } = Metadata.new(timestamp)
     end
