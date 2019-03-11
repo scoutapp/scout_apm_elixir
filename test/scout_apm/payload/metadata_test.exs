1eaceff6f8275e79b7c54847bc57f407d8404374
Chris Schneider <chris@scoutapp.com>
2019-03-11 10:55:07 -0600

    Don't hard code version in tests

--- test/scout_apm/payload/metadata_test.exs
+++ test/scout_apm/payload/metadata_test.exs
@@ -7,9 +7,10 @@ defmodule ScoutApm.Payload.MetadataTest do
     test "creating a metadata struct with a timestamp" do
       {:ok, app_root} = File.cwd()
       {:ok, timestamp} = NaiveDateTime.new(2018, 1, 1, 1, 5, 3)
+      agent_version = Application.spec(:scout_apm)[:vsn] |> to_string()
 
       assert %ScoutApm.Payload.Metadata{
-               agent_version: "0.4.11",
+               agent_version: ^agent_version,
                app_root: ^app_root,
                payload_version: 1,
                agent_time: "2018-01-01T01:05:03Z",
