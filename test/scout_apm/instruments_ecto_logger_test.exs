922b0eff303a6ec7ed169bac870fafe8ed15238d
Mitchell Henke <mitchell@mitchellhenke.com>
2019-06-04 13:01:19 -0500

    fix Ecto 2 instrumentation

--- test/scout_apm/instruments_ecto_logger_test.exs
+++ test/scout_apm/instruments_ecto_logger_test.exs
@@ -1,7 +1,7 @@
 defmodule ScoutApm.Instruments.EctoLoggerTest do
   use ExUnit.Case, async: false
 
-  describe "record/1" do
+  describe "record/2" do
     test "successfully records query" do
       value = %{
         decode_time: 16000,
@@ -20,6 +20,30 @@ defmodule ScoutApm.Instruments.EctoLoggerTest do
         children: [[
           %{
             name: "SQL#users",
+            type: "Ecto",
+          }
+        ]]
+      } = Process.get(:scout_apm_request)
+    end
+  end
+
+  describe "log/1" do
+    test "successfully records query" do
+      entry = %{
+        decode_time: 16000,
+        query_time: 1192999,
+        queue_time: 36000,
+        result: {:ok, %{__struct__: Postgrex.Result, command: :select}},
+        source: "users",
+        query: "SELECT u0.\"id\", u0.\"name\", u0.\"age\" FROM \"users\" AS u0",
+      }
+
+      ScoutApm.TrackedRequest.start_layer("Controller", "test")
+      ScoutApm.Instruments.EctoLogger.log(entry)
+      assert %{
+        children: [[
+          %{
+            name: "select#users",
             type: "Ecto",
           }
         ]]
