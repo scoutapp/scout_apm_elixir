0f21c69581c3551e83cf133e651941ac199b1371
Mitchell Henke <mitchell@mitchellhenke.com>
2019-08-27 15:00:22 -0500

    use Jason instead of Poison

--- config/config.exs
+++ config/config.exs
@@ -20,6 +20,7 @@ use Mix.Config
 #
 #     config :logger, level: :info
 #
+config :phoenix, :json_library, Jason
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
