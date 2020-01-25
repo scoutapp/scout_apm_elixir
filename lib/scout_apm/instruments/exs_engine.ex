6b89acea046ea9dc402270986f528d00e85623b2
Derek Schneider <dschneider.641@gmail.com>
2020-01-24 21:57:58 -0500

    Exs engine: use full path for name.

--- lib/scout_apm/instruments/exs_engine.ex
+++ lib/scout_apm/instruments/exs_engine.ex
@@ -3,21 +3,11 @@ defmodule ScoutApm.Instruments.ExsEngine do
 
   # TODO: Make this name correctly for other template locations
   def compile(path, name) do
-    # web/templates/page/index.html.eex
-    scout_name =
-      path
-      # [web, templates, page, index.html.eex]
-      |> String.split("/")
-      # [page, index.html.eex]
-      |> Enum.drop(2)
-      # "page/index.html.eex"
-      |> Enum.join("/")
-
     quoted_template = Phoenix.Template.ExsEngine.compile(path, name)
 
     quote do
       require ScoutApm.Tracing
-      ScoutApm.Tracing.timing("Exs", unquote(scout_name), do: unquote(quoted_template))
+      ScoutApm.Tracing.timing("Exs", unquote(path), do: unquote(quoted_template))
     end
   end
 end
