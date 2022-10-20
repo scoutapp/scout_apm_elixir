ac328cc0fab8335fe50296306be5a0f8cb2c4717
Thuyen Phan <vanthuyen.phan@gmail.com>
2022-10-20 18:14:50 +0800

    Upgrade to Elixir 1.14 and upgrade deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ScoutApm.Mixfile do
     [
       app: :scout_apm,
       version: "1.0.7",
-      elixir: "~> 1.4",
+      elixir: "~> 1.14",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
